a43c8a2e0ebb6874b5454254aadec26472124dd3
drevanth095-maker <drevanth095@gmail.com>
2026-03-15 13:32:31 +0530

    updated k means cluster

--- src/clustering/k means.py
+++ src/clustering/k means.py
@@ -1,16 +1,10 @@
 from sklearn.preprocessing import StandardScaler
-
 X = df.drop('heart_disease', axis=1)
-
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 #---- K Means-----
 from sklearn.cluster import KMeans
-
 kmeans = KMeans(n_clusters=3, random_state=0)
-
 kmeans_labels = kmeans.fit_predict(X_scaled)
-
 df['KMeans_cluster'] = kmeans_labels
-
 print(df['KMeans_cluster'].value_counts())
