db1b7f27b32e13b2b3dd972b19231ba6bb6012f0
drevanth095-maker <drevanth095@gmail.com>
2026-03-15 15:30:16 +0530

    Implementation of hierarchical clustering

--- src/clustering/Hierarchical.py
+++ src/clustering/Hierarchical.py
@@ -1,9 +1,5 @@
 from scipy.cluster.hierarchy import linkage, fcluster
-
 Z = linkage(X_scaled, method='ward')
-
 clusters = fcluster(Z, 3, criterion='maxclust')
-
 df['Hierarchical_cluster'] = clusters
-
 print(df['Hierarchical_cluster'].value_counts())
