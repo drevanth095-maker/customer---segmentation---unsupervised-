f6b4b3b49ca2eb2cd517852c67e7a7839453e723
drevanth095-maker <drevanth095@gmail.com>
2026-03-15 15:00:58 +0530

    implementation of DBSCAN for clustering

--- src/clustering/DBSCAN.py
+++ src/clustering/DBSCAN.py
@@ -1,9 +1,5 @@
 from sklearn.cluster import DBSCAN
-
 dbscan = DBSCAN(eps=1.5, min_samples=10)
-
 labels = dbscan.fit_predict(X_scaled)
-
 df['DBSCAN_cluster'] = labels
-
 print(df['DBSCAN_cluster'].value_counts())
