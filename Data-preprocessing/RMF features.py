6f94fceb188d9597535973fa8dc88d671edcce16
drevanth095-maker <drevanth095@gmail.com>
2026-03-15 22:00:42 +0530

    Implemetation of RMF features in data preprocessing

--- Data-preprocessing/RMF features.py
+++ Data-preprocessing/RMF features.py
@@ -1,11 +1,8 @@
 # Recency
 df['recency'] = df['age'].max() - df['age']
-
-# Frequency
 df['frequency'] = df['exang'] + df['fbs']
-
-# Monetary
 df['monetary'] = df['trestbps'] + df['chol']
-#Behavioral Features
 df['lifestyle_risk'] = df['smoking'] + df['diabetes']
 df['exercise_risk'] = df['exang'] + df['oldpeak']
+df['chol_bmi_ratio'] = df['chol'] / df['bmi']
+df['bp_bmi_ratio'] = df['trestbps'] / df['bmi']
